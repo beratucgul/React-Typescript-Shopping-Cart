06219d736295cadcfa198bbfb83af931f4d8a2e2
JBerat <berat.99@hotmail.com>
2022-10-14 17:08:58 +0300

    increase cart quantity context is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import ContainerComponent from "./components/ContainerComponent";
 import Navbar from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <ContainerComponent />;
-    </>
+    </ShoppingCartProvider>
   );
 }
 
