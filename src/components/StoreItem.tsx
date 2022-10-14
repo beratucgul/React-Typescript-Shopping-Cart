8ecf24192dcab330f2923b8eea1450b699731a57
JBerat <berat.99@hotmail.com>
2022-10-14 16:22:13 +0300

    currency format is added

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { FormatCurrency } from "../utilites/FormatCurrency";
 
 type storeItemProps = {
   id: number;
@@ -16,6 +17,12 @@ export function StoreItem({ id, name, price, imgUrl }: storeItemProps) {
         height="200px"
         style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{FormatCurrency(price)}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 }
